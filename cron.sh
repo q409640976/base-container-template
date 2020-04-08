7fd00c3fc6e29b57d20baf2cf508bdba751229e1
q409640976 <q409640976@gmail.com>
2020-04-08 20:48:43 +0800

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -6,9 +6,12 @@ process=$1
 pid=$(ps -ef | grep simplewrapper | grep -v grep | wc -l)
 echo `date` >> /headless/cron.log 
 if [ $pid -eq 0 ]; then
+ echo "hit not tun" >> /headless/cron.log 
+ cd  /headless/hitleap
  nohup /headless/hitleap/HitLeap-Viewer.desktop &
 fi
 cp=$(du -hs /tmp)
 cp=${cp%/*}
 myip=$(/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:")
 curl http://update.limou.cc/hit/add.php?cnt=$pid\&name=${myip}--$cp
+echo "over" >> /headless/cron.log 
